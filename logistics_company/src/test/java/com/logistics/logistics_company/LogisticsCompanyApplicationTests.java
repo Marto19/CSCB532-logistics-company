a2c31c971bb5f1d74d68d89f88b0685a3f8a80fb
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-11-28 16:14:17 +0200

    adding the spring project for web app
    dependency added - spring Web.
    Build web, including RESTful, applications using Spring MVC. Uses Apache Tomcat as the default embedded container.

---
+++ logistics_company/src/test/java/com/logistics/logistics_company/LogisticsCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.logistics_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
