b385f216c3ba5e2811af2b31680d0a1d3bf6fd69
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2023-11-28 17:39:15 +0200

    moved the project in the right place
    there was an error :java file outside the source group
    
    which prevented the main method to run

---
+++ src/main/java/com/logistics/logisticsCompany/LogisticsCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.logisticsCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistics/logisticsCompany/LogisticsCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.logisticsCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
