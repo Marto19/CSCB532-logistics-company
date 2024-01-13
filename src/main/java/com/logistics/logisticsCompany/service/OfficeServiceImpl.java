746eaf3e6898f153f36156076e037839d930ecff
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2024-01-13 19:37:04 +0200

    Update OfficeServiceImpl.java

--- src/main/java/com/logistics/logisticsCompany/service/OfficeServiceImpl.java
+++ src/main/java/com/logistics/logisticsCompany/service/OfficeServiceImpl.java
@@ -1,18 +1,18 @@
-package com.logistics.logisticsCompany.service;
-
-import com.logistics.logisticsCompany.entities.Office;
-import com.logistics.logisticsCompany.repository.OfficeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
- public class OfficeServiceImpl {
-
-    @Autowired
-    private OfficeRepository officeRepository;
-    public void createOffice(Office office) {
-        officeRepository.save(office);
-    }
-    //business logic goes here
-
-}
+//package com.logistics.logisticsCompany.service;
+//
+//import com.logistics.logisticsCompany.entities.Office;
+//import com.logistics.logisticsCompany.repository.OfficeRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+// public class OfficeServiceImpl {
+//
+//    @Autowired
+//    private OfficeRepository officeRepository;
+//    public void createOffice(Office office) {
+//        officeRepository.save(office);
+//    }
+//    //business logic goes here
+//
+//}
