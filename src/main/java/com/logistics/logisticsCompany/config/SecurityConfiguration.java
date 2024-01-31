281174cee4ad68683e98bb0be8fc242fb6d35fd1
Martin Trenkov <101531043+Marto19@users.noreply.github.com>
2024-01-31 21:11:33 +0200

    setting access points for each role

--- src/main/java/com/logistics/logisticsCompany/config/SecurityConfiguration.java
+++ src/main/java/com/logistics/logisticsCompany/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,13 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers( "/api/v1/auth/**", "/login", "/register")   //todo: change whitelist pages
                         .permitAll()
+                        //TODO: FETCH THE ROLES PROPERLY, MAYBE WHEN LOGIN?
+                                .requestMatchers(HttpMethod.POST, "/api/v1/users/assign-roles").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/logistics-companies/**").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/employees/**").hasRole("EMPLOYEE")
+                                .requestMatchers("/api/v1/users/**").hasRole("USER")
+                                .requestMatchers("/api/v1/customers/**").hasRole("CUSTOMER")
+                                .requestMatchers("/api/v1/shipments/**").hasRole("EMPLOYEE")
                         .anyRequest()
                         .authenticated()
                         //.and() of course .and() is deprecated, because why not
@@ -39,3 +47,75 @@ public class SecurityConfiguration {
         return http.build();
     }
 }
+
+//        1. Register/Login Requests:
+//
+//        Role: USER
+//        Endpoints:
+//        POST /api/v1/users/login
+//        POST /api/v1/users/register
+//        POST /api/v1/auth/authenticate
+//
+//        2. Set User Roles Requests:
+//
+//        Role: ADMIN
+//        Endpoints:
+//        POST /api/v1/users/assign-roles
+//
+//        3. Logistics Company CRUD Requests:
+//        a) Logistics Company:
+//
+//        Role: ADMIN
+//        Endpoints:
+//        POST /api/v1/logistics-companies
+//        GET /api/v1/logistics-companies
+//        GET /api/v1/logistics-companies/{id}
+//        PUT /api/v1/logistics-companies/{id}
+//        DELETE /api/v1/logistics-companies/{id}
+//
+//        b) Employee CRUD Requests:
+//
+//        Role: EMPLOYEE
+//        Endpoints:
+//        POST /api/v1/employees
+//        GET /api/v1/employees
+//        GET /api/v1/employees/{id}
+//        PUT /api/v1/employees/{id}
+//        DELETE /api/v1/employees/{id}
+//
+//        c) User CRUD Requests:
+//
+//        Role: USER
+//        Endpoints:
+//        POST /api/v1/users
+//        GET /api/v1/users
+//        GET /api/v1/users/{id}
+//        PUT /api/v1/users/{id}
+//        DELETE /api/v1/users/{id}
+//
+//        d) Customer CRUD Requests:
+//
+//        Role: CUSTOMER
+//        Endpoints:
+//        POST /api/v1/customers
+//        GET /api/v1/customers
+//        GET /api/v1/customers/{id}
+//        PUT /api/v1/customers/{id}
+//        DELETE /api/v1/customers/{id}
+//
+//        4. Register Sent/Received Goods CRUD Requests:
+//
+//        Role: EMPLOYEE
+//        Endpoints:
+//        POST /api/v1/shipments/sent
+//        POST /api/v1/shipments/received
+//        GET /api/v1/shipments
+//        GET /api/v1/shipments/{id}
+//        GET /api/v1/shipments/employee/{employeeId}
+//        PUT /api/v1/shipments/{id}
+//        DELETE /api/v1/shipments/{id}
+//        Role: USER
+//
+//        Endpoints:
+//        GET /api/v1/users/{userId} (To fetch details of their own profile)
+//        GET /api/v1/shipments/employee/{employeeId}(To fetch their own order and stuff idk)
